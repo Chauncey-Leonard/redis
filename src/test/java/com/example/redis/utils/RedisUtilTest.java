830ab9e2a50e17d64fbffe98c912055944070097
chaunceyleonard <1723446@qq.com>
2021-01-24 13:35:48 +0800

    feat：Redis测试类

---
+++ src/test/java/com/example/redis/utils/RedisUtilTest.java
@@ -0,0 +1,10 @@
+package com.example.redis.utils;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisUtilTest {
+    public static void main(String[] args) {
+        // 1、创建Jedis对象
+        Jedis jedis = new Jedis();
+    }
+}
