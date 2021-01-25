70d7bb0f080925486da8151893b13be63ca13f01
Chauncey-Leonard <1723446@qq.com>
2021-01-25 21:34:43 +0800

    feat: String新增键值对

---
+++ src/main/java/com/example/redis/utils/TestString.java
@@ -0,0 +1,19 @@
+package com.example.redis.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+
+/**
+ * String
+ */
+@Slf4j
+public class TestString {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+
+        // 新增
+        System.out.println(jedis.set("firstName", "Chauncey"));
+        System.out.println(jedis.set("lastName", "Leonard"));
+        System.out.println(jedis.set("fullName", "Chauncey Leonard"));
+    }
+}
