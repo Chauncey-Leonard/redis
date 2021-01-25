004c1a72ff5fa6bc068f41e371b9560231a5fd67
Chauncey-Leonard <1723446@qq.com>
2021-01-25 21:43:29 +0800

    feat: String多个键值对的操作

--- src/main/java/com/example/redis/utils/TestString.java
+++ src/main/java/com/example/redis/utils/TestString.java
@@ -26,7 +26,16 @@ public class TestString {
         // System.out.println(jedis.set("lastName", "Tang"));
 
         // 在fullName值后追加值
-        System.out.println(jedis.append("fullName", "appendValue"));
-        System.out.println(jedis.get("fullName"));
+        // System.out.println(jedis.append("fullName", "appendValue"));
+        // System.out.println(jedis.get("fullName"));
+
+        // 新增多个键值对
+        // System.out.println(jedis.mset("key1", "value1", "key2", "value2"));
+
+        // 获取多个键值对
+        // System.out.println(jedis.mget("key1", "key2"));
+
+        // 删除多个键值对
+        System.out.println(jedis.del("key1", "key2"));
     }
 }
