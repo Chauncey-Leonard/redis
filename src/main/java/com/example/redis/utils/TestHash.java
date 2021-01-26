4ac5e4478badeb355a4251dd49d47b6f185dbb1f
Chauncey <1723446@qq.com>
2021-01-26 09:16:45 +0800

    feat: hash批量获取属性值

--- src/main/java/com/example/redis/utils/TestHash.java
+++ src/main/java/com/example/redis/utils/TestHash.java
@@ -2,6 +2,9 @@ package com.example.redis.utils;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestHash {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("127.0.0.1", 6379);
@@ -28,5 +31,16 @@ public class TestHash {
         System.out.println(jedis.hkeys("fullName"));
         // 获取指定键的属性数量
         System.out.println(jedis.hlen("fullName"));
+
+        // 批量设置属性值
+        Map<String, String> hash = new HashMap<>();
+        hash.put("field1", "value1");
+        hash.put("field2", "value2");
+        hash.put("field3", "value3");
+        hash.put("field4", "value4");
+        jedis.hmset("myHash", hash);
+
+        // 批量获取属性值
+        System.out.println(jedis.hmget("myHash", "field1", "field2"));
     }
 }
