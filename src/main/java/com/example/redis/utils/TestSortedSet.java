601d256c06b57e05e78583d8de1c5ef54312f9f1
Chauncey-Leonard <1723446@qq.com>
2021-01-26 21:23:15 +0800

    feat: zset新增一个或多个元素至排序集合

---
+++ src/main/java/com/example/redis/utils/TestSortedSet.java
@@ -0,0 +1,15 @@
+package com.example.redis.utils;
+
+import redis.clients.jedis.Jedis;
+
+public class TestSortedSet {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.flushDB();
+
+        // 新增一个或多个元素至排序集合
+        System.out.println(jedis.zadd("myzset", 1, "one"));
+        System.out.println(jedis.zadd("myzset", 1, "uno"));
+        System.out.println(jedis.zadd("myzset", 2, "two"));
+    }
+}
