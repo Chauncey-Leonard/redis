1cf5b606d70b4a7cb36050e3492c6633a050e4b8
Chauncey-Leonard <1723446@qq.com>
2021-01-28 21:54:54 +0800

    feat: redis自定义RedisTemplate

---
+++ springboot-redis/src/main/java/com/example/redis/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.example.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    // 编写自己的 RedisTemplate
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+}
