443e136e59f53b1fd480b098d90e7af890b70e41
Chauncey-Leonard <1723446@qq.com>
2021-01-28 21:47:35 +0800

    feat: redis测试

--- springboot-redis/src/test/java/com/example/redis/SpringbootRedisApplicationTests.java
+++ springboot-redis/src/test/java/com/example/redis/SpringbootRedisApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.redis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Objects;
 
 @SpringBootTest
 class SpringbootRedisApplicationTests {
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void redisTemplateTest() {
+        /*
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         *
+         * redisTemplate 操作不同的数据类型
+         * opsForValue String
+         * opsForList
+         * opsForHash
+         * opsForSet
+         * opsForZSet
+         * opsForGeo
+         * opsForHyperLogLog
+         *
+         * 除了基本的数据类型操作，其他常用的方法都可以通过redisTemplate直接操作
+         * 比如，事务、和基本的CRUD
+         *
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         */
+
+        RedisConnection connection = Objects.requireNonNull(redisTemplate.getConnectionFactory())
+                .getConnection();
+        connection.flushDb();
+        connection.flushAll();
+
+        redisTemplate.opsForValue().set("name", "Chauncey");
+        System.out.println(redisTemplate.opsForValue().get("name"));
     }
 
 }
