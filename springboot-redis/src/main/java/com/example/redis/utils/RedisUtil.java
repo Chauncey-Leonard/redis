6cf359efd5744bed99d063a60dc445919b04d37e
Chauncey-Leonard <1723446@qq.com>
2021-01-30 13:56:26 +0800

    feat: Redis工具类指定失效时间

---
+++ springboot-redis/src/main/java/com/example/redis/utils/RedisUtil.java
@@ -0,0 +1,33 @@
+package com.example.redis.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 指定缓存失效时间
+     *
+     * @param key  键
+     * @param time 时间(秒)
+     * @return 指定失效时间是否成功
+     */
+    public boolean expire(String key, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
